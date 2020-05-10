96eda2fbf674642ae005110538e620b0261744c0
bryanchoi <bryanxchoi@gmail.com>
2020-05-10 00:01:33 -0700

    finished writing Dictionaries for course information. -- look into non case sensitive generic dictionary for strings

--- CourseInformation/Form1.Designer.cs
+++ CourseInformation/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.btnEnter = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtCourseNumberInput = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -38,37 +38,40 @@
             this.lblInstructor = new System.Windows.Forms.Label();
             this.lblRoomNumber = new System.Windows.Forms.Label();
             this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnEnter
             // 
-            this.btnEnter.Location = new System.Drawing.Point(286, 44);
+            this.btnEnter.Location = new System.Drawing.Point(298, 31);
             this.btnEnter.Name = "btnEnter";
             this.btnEnter.Size = new System.Drawing.Size(75, 23);
             this.btnEnter.TabIndex = 0;
             this.btnEnter.Text = "Enter";
             this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 49);
+            this.label1.Location = new System.Drawing.Point(44, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(86, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Course Number: ";
             // 
-            // textBox1
+            // txtCourseNumberInput
             // 
-            this.textBox1.Location = new System.Drawing.Point(135, 46);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(124, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtCourseNumberInput.Location = new System.Drawing.Point(136, 34);
+            this.txtCourseNumberInput.Name = "txtCourseNumberInput";
+            this.txtCourseNumberInput.Size = new System.Drawing.Size(142, 20);
+            this.txtCourseNumberInput.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(65, 118);
+            this.label2.Location = new System.Drawing.Point(13, 31);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(81, 13);
             this.label2.TabIndex = 3;
@@ -77,7 +80,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(92, 158);
+            this.label3.Location = new System.Drawing.Point(40, 71);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 4;
@@ -86,7 +89,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(69, 199);
+            this.label4.Location = new System.Drawing.Point(17, 112);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(77, 13);
             this.label4.TabIndex = 5;
@@ -95,7 +98,7 @@
             // lblMeetingTime
             // 
             this.lblMeetingTime.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblMeetingTime.Location = new System.Drawing.Point(150, 193);
+            this.lblMeetingTime.Location = new System.Drawing.Point(98, 106);
             this.lblMeetingTime.Name = "lblMeetingTime";
             this.lblMeetingTime.Size = new System.Drawing.Size(106, 27);
             this.lblMeetingTime.TabIndex = 8;
@@ -103,7 +106,7 @@
             // lblInstructor
             // 
             this.lblInstructor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblInstructor.Location = new System.Drawing.Point(150, 152);
+            this.lblInstructor.Location = new System.Drawing.Point(98, 65);
             this.lblInstructor.Name = "lblInstructor";
             this.lblInstructor.Size = new System.Drawing.Size(106, 27);
             this.lblInstructor.TabIndex = 7;
@@ -111,14 +114,14 @@
             // lblRoomNumber
             // 
             this.lblRoomNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblRoomNumber.Location = new System.Drawing.Point(150, 112);
+            this.lblRoomNumber.Location = new System.Drawing.Point(98, 25);
             this.lblRoomNumber.Name = "lblRoomNumber";
             this.lblRoomNumber.Size = new System.Drawing.Size(106, 27);
             this.lblRoomNumber.TabIndex = 6;
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(160, 230);
+            this.btnExit.Location = new System.Drawing.Point(298, 201);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 9;
@@ -126,23 +129,34 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblMeetingTime);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblInstructor);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.lblRoomNumber);
+            this.groupBox1.Location = new System.Drawing.Point(47, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(231, 152);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(421, 265);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.lblMeetingTime);
-            this.Controls.Add(this.lblInstructor);
-            this.Controls.Add(this.lblRoomNumber);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtCourseNumberInput);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnEnter);
             this.Name = "Form1";
             this.Text = "Course Information";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,7 +166,7 @@
 
         private System.Windows.Forms.Button btnEnter;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtCourseNumberInput;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
@@ -160,6 +174,7 @@
         private System.Windows.Forms.Label lblInstructor;
         private System.Windows.Forms.Label lblRoomNumber;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- CourseInformation/Form1.cs
+++ CourseInformation/Form1.cs
@@ -12,6 +12,33 @@ namespace CourseInformation
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, string> roomNumbers = new Dictionary<string, string>()
+        {
+            {"CS101", "3004" },
+            {"CS102", "4501" },
+            {"CS103", "6755" },
+            {"NT110", "1244" },
+            {"CM241", "1411" }
+        };
+
+        private Dictionary<string, string> instructors = new Dictionary<string, string>()
+        {
+            {"CS101", "Haynes" },
+            {"CS102", "Alvarado" },
+            {"CS103", "Rich" },
+            {"NT110", "Burke" },
+            {"CM241", "Lee" }
+        };
+
+        private Dictionary<string, string> meetingTimes = new Dictionary<string, string>()
+        {
+            {"CS101", "8:00 AM" },
+            {"CS102", "9:00 AM" },
+            {"CS103", "10:00 AM" },
+            {"NT110", "11:00 AM" },
+            {"CM241", "1:00 AM" }
+        };
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +47,32 @@ namespace CourseInformation
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string courseNumber = "";
+                string roomNumber = "";
+                string instructor = "";
+                string meetingTime = "";
+
+                courseNumber = txtCourseNumberInput.Text;
+
+                roomNumber = roomNumbers[courseNumber];
+                instructor = instructors[courseNumber];
+                meetingTime = meetingTimes[courseNumber];
+
+                lblRoomNumber.Text = roomNumber;
+                lblInstructor.Text = instructor;
+                lblMeetingTime.Text = meetingTime;
+            }
+            catch
+            {
+                // make non case sensitive generic dictionary for strings?
+                MessageBox.Show("Enter a valid course number. Case-senstive.");
+            }
         }
     }
 }
